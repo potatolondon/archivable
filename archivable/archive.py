1c307ab39bc1ecd1947a1827897be3b9527d630d
Luke Benstead <lukeb@potatolondon.com>
2015-10-13 13:10:15 +0100

    Initial commit

---
+++ archivable/archive.py
@@ -0,0 +1,112 @@
+from django.db import models
+from django.dispatch import Signal
+
+
+def _add_field_to_class(cls, name):
+    try:
+        pk_field = (x for x in cls._meta.fields if x.primary_key).next()
+        pk_type = pk_field.__class__
+    except StopIteration:
+        pk_type = None
+
+    if pk_type is None:
+        pk_type = models.AutoField
+
+    if pk_type == models.CharField:
+        models.CharField(
+            editable=False,
+            max_length=pk_field.max_length,
+            blank=True,
+            default=""
+        ).contribute_to_class(cls, name)
+    else:
+        models.BigIntegerField(
+            editable=False,
+            blank=True,
+            default=0
+        ).contribute_to_class(cls, name)
+
+
+def _add_field_to_unique_constraints(cls, name):
+    new_unique_togethers = []
+
+    for field in cls._meta.fields:
+        if field.unique and not field.primary_key:
+            field._unique = False
+
+            new_unique_togethers.append((field.name, "archive_identifier"))
+
+    for constraint in cls._meta.unique_together:
+        new_unique_togethers.append(
+            tuple(list(constraint) + ["archive_identifier"])
+        )
+
+    cls._meta.unique_together = new_unique_togethers
+
+
+def _replace_manager(cls, new_manager_name):
+    if hasattr(cls, "objects"):
+        default_manager = cls.objects.__class__
+    else:
+        default_manager = models.Manager
+
+    class ExcludeArchivedManager(default_manager):
+        use_in_migrations = False
+
+        def get_queryset(self):
+            return super(ExcludeArchivedManager, self).get_queryset().filter(archive_identifier=0)
+
+    default_manager().contribute_to_class(cls, "with_archived")
+    ExcludeArchivedManager().contribute_to_class(cls, "objects")
+    cls._default_manager = cls.objects
+
+
+post_archive = Signal(providing_args=["instance"])
+
+
+def _override_methods(cls):
+    def archive(self):
+        self.archive_identifier = self.pk
+        self.save()
+        post_archive.send(sender=self.__class__, instance=self)
+
+    def restore(self):
+        self.archive_identifier = 0
+        self.save()
+
+    def delete(self, force=False, *args, **kwargs):
+        if force:
+            return super(cls, self).delete(*args, **kwargs)
+        else:
+            self.archive()
+
+    @property
+    def is_archived(self):
+        return self.archive_identifier != 0
+
+    cls.archive = archive
+    cls.restore = restore
+    cls.delete = delete
+    cls.is_archived = is_archived
+
+
+def archivable(cls):
+    """
+        A class decorator that makes a model archivable
+
+        Works by:
+
+         - Adding an archive_identifier field, which if zero means the object is unarchived, otherwise
+           it's made equal to the instance's PK
+         - Manipulates unique constraints to add the archive_identifier
+         - Overrides delete to instead archive unless you pass force=True as a param
+         - Adds including_archived as a manager
+         - Replaces objects with ExcludeArchivedManager
+    """
+
+    _add_field_to_class(cls, "archive_identifier")
+    _add_field_to_unique_constraints(cls, "archive_identifier")
+    _replace_manager(cls, "with_archived")
+    _override_methods(cls)
+
+    return cls
---
+++ archivable/tests.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.test import TestCase
+import mock
+
+from . import archivable
+from .archive import _add_field_to_class, _replace_manager
+
+
+@archivable
+class ArchiveModel(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class ArchiveTests(TestCase):
+
+    @mock.patch.object(ArchiveModel, "save")
+    def test_achive_sets_archive_identifier(self, mock_save):
+        instance = ArchiveModel(name="one", pk=1)
+        self.assertEqual(0, instance.archive_identifier)
+        instance.archive()
+        self.assertEqual(instance.pk, instance.archive_identifier)
+
+    @mock.patch.object(ArchiveModel, "save")
+    def test_delete_archives(self, mock_save):
+        instance = ArchiveModel(name="one", id=1, pk=1)
+        instance.delete()
+        self.assertEqual(instance.pk, instance.archive_identifier)
+
+    @mock.patch.object(ArchiveModel, "delete")
+    def test_delete_deletes_with_force(self, mock_delete):
+        instance = ArchiveModel(name="one", id=1, pk=1)
+        instance.delete(force=True)
+        self.assertNotEqual(instance.pk, instance.archive_identifier)
+
+    @mock.patch.object(ArchiveModel, "save")
+    def test_restore_restores(self, mock_save):
+        instance = ArchiveModel(name="one", pk=1, id=1)
+
+        instance.archive()
+        self.assertEqual(instance.pk, instance.archive_identifier)
+
+        instance.restore()
+        self.assertNotEqual(instance.pk, instance.archive_identifier)
+
+    def test_archive_identifier_matches_pk_type(self):
+        @archivable
+        class IntModel(models.Model):
+            id = models.AutoField(primary_key=True)
+
+        @archivable
+        class CharModel(models.Model):
+            id = models.CharField(primary_key=True, max_length=10)
+
+        @archivable
+        class NoSpecificPK(models.Model):
+            pass
+
+        self.assertEqual(models.BigIntegerField, IntModel._meta.get_field("archive_identifier").__class__)
+        self.assertEqual(models.BigIntegerField, NoSpecificPK._meta.get_field("archive_identifier").__class__)
+        self.assertEqual(models.CharField, CharModel._meta.get_field("archive_identifier").__class__)
+        self.assertEqual(10, CharModel._meta.get_field("archive_identifier").max_length)
+
+        # testing scenario when we have StopIteration raised
+        ArchiveModel._meta.fields = []
+        _add_field_to_class(ArchiveModel, 'pk_archive')
+        self.assertEquals(models.BigIntegerField, ArchiveModel._meta.get_field("pk_archive").__class__)
+
+    def test_replace_manager_use_default(self):
+        del ArchiveModel.objects
+        _replace_manager(ArchiveModel, 'some_manager')
+        self.assertEquals(models.Manager, getattr(ArchiveModel, "with_archived").__class__)
+
+    def test_unique_together(self):
+        @archivable
+        class UniqueTogetherModel(models.Model):
+            name = models.CharField(max_length=100)
+            other_name = models.CharField(max_length=100)
+
+            class Meta:
+                unique_together = (('name', 'other_name'),)
+
+        self.assertFalse(('name', 'other_name') in UniqueTogetherModel._meta.unique_together)
+        self.assertTrue(('name', 'other_name', 'archive_identifier') in UniqueTogetherModel._meta.unique_together)
+
+    def test_is_archived_property(self):
+        instance = ArchiveModel(name="one", pk=1)
+        self.assertFalse(instance.is_archived)
+        instance.archive_identifier = 1
+        self.assertTrue(instance.is_archived)
